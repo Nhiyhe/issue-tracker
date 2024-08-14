3f65ec6f6aa0209f730047cd32f4d9b14fa496a6
Funminiyi Adekunle <nhiyhe@gmail.com>
2024-08-14 13:07:30 +0100

    update issue

--- src/app/issues/_components/IssueForm.tsx
+++ src/app/issues/_components/IssueForm.tsx
@@ -29,7 +29,9 @@ const NewIssueForm = ({ issue }: { issue?: Issue }) => {
 
   const onSubmit = handleSubmit(async (data) => {
     try {
-      await axios.post("/api/issues", data);
+      if (issue) {
+        await axios.patch(`/api/issues/${issue.id}`, data);
+      } else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setError("An unexpected error occured.");
@@ -55,7 +57,8 @@ const NewIssueForm = ({ issue }: { issue?: Issue }) => {
       <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
       <Button disabled={isSubmitting}>
-        Submit New Issue {isSubmitting && <Spinner />}
+        {issue ? "Update Issue" : "Submit New Issue"}{" "}
+        {isSubmitting && <Spinner />}
       </Button>
     </form>
   );
