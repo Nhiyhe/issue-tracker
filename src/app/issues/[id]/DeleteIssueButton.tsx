3defcad797a84f1da984d636e4da74f0ab190254
Funminiyi Adekunle <nhiyhe@gmail.com>
2024-08-18 23:11:20 +0100

    add confirmation dialog

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,31 @@
 import { Cross2Icon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <Cross2Icon />
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <Cross2Icon />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue?. This is action cannot be
+          reversed.
+        </AlertDialog.Description>
+        <Flex className="mt-4" gap="4">
+          <AlertDialog.Cancel>
+            <Button color="gray">Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
